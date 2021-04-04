1c9320b8da6c5d59f5baa7e126697fe85baaf6fd
Artyom Kuzkov <mc.ky3r@gmail.com>
2021-04-04 19:59:43 +0300

    Investigating chakra ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,38 @@
-import { Button, useColorMode } from '@chakra-ui/react';
+import {
+  Box,
+  Container,
+  Text,
+  Heading,
+  useColorMode,
+  VStack,
+  HStack,
+  Button,
+} from '@chakra-ui/react';
+import React from 'react';
 
 const IndexPage = () => {
-  const { toggleColorMode } = useColorMode();
-
   return (
-    <Button size="sm" onClick={toggleColorMode}>
-      Toggle Mode
-    </Button>
+    <Container maxW="container.md" mt="8">
+      <Heading>Hello there! I'm a professional frontend engineer</Heading>
+      <Text mt="4">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.Consequuntur met
+        eu animi, sit vel id quae esse illum!Similique dolor sunt atque
+        cupiditate nostrum distinctio quia fuga explicabo quae! Lorem ipsum
+        dolor sit, amet consectetur adipisicing elit. Qui dicta eius autem
+        temporibus beatae quo error ut amet. Cupiditate obcaecati voluptas
+        cumque hic ipsum, autem impedit eaque asperiores blanditiis aut. Lorem
+        ipsum dolor sit amet consectetur adipisicing elit. Enim fuga sed
+        distinctio dolores doloremque omnis ut. Numquam dolorum, provident,
+        adipisci incidunt at eos placeat ea illo vel, quaerat voluptatibus
+        rerum!
+      </Text>
+      <HStack mt="4">
+        <Button colorScheme="teal">Contact me</Button>
+        <Button colorScheme="teal" variant="ghost">
+          Learn more
+        </Button>
+      </HStack>
+    </Container>
   );
 };
 
