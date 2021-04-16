6b2fbb1b8c0339c75d57d8d3e26dc1ac59cd8177
Artyom Kuzkov <mc.ky3r@gmail.com>
2021-04-16 08:48:39 +0300

    Finish

--- docs/_next/static/oteAXmJg1owzivnkbH0E-/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-0b98961377e7960eece0.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-e57de6eeb34803423b21.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,62 @@
-import { Container, Text, Heading, HStack, Button } from '@chakra-ui/react';
+import {
+  Container,
+  Text,
+  Heading,
+  HStack,
+  Button,
+  Box,
+  Link,
+} from '@chakra-ui/react';
 import React from 'react';
 
 const IndexPage = () => {
   return (
-    <Container maxW="container.md" mt="8">
-      <Heading>Hello there! I'm a professional frontend engineer</Heading>
-      <Text mt="4">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit.Consequuntur met
-        eu animi, sit vel id quae esse illum!Similique dolor sunt atque
-        cupiditate nostrum distinctio quia fuga explicabo quae! Lorem ipsum
-        dolor sit, amet consectetur adipisicing elit. Qui dicta eius autem
-        temporibus beatae quo error ut amet. Cupiditate obcaecati voluptas
-        cumque hic ipsum, autem impedit eaque asperiores blanditiis aut. Lorem
-        ipsum dolor sit amet consectetur adipisicing elit. Enim fuga sed
-        distinctio dolores doloremque omnis ut. Numquam dolorum, provident,
-        adipisci incidunt at eos placeat ea illo vel, quaerat voluptatibus
-        rerum!
-      </Text>
-      <HStack mt="4">
-        <Button colorScheme="teal">Contact me</Button>
-        <Button colorScheme="teal" variant="ghost">
-          Learn more
-        </Button>
-      </HStack>
+    <Container maxW="container.lg" mt="8">
+      <Box mt="24" px={[4, 4, 8]} w={['100%', '100%', '70%']}>
+        <Heading>Hello there! I'm a professional frontend engineer</Heading>
+        <Text mt="4">
+          I’m a hard working and dedicated professional front-end developer/user
+          interface designer with a keen eye for detail, and a determination to
+          deliver the very highest quality. I take great pride in my work, and I
+          always try to better myself with every project I work on.
+        </Text>
+        <Text mt="2">
+          I like to write semantic, cross browser compatible code by hand that
+          is re-useable, maintainable and easy to understand.
+        </Text>
+        <Text mt="2">
+          The main areas of my expertise are React, Angular and JavaScript but I
+          have intermediate knowledge of Vue and Svelte.
+        </Text>
+        <Text mt="2">
+          I have worked with a number of high profile clients (both directly and
+          indirectly) and I am comfortable working under pressure to complex
+          specifications.
+        </Text>
+        <Text mt="2">
+          <Text fontWeight="bold">Interested in working with me?</Text>
+          You can contact me in{' '}
+          <Link color="teal.500" href="https://t.me/artyom_kuzkov" isExternal>
+            telegram
+          </Link>{' '}
+          or{' '}
+          <Link color="teal.500" href="mailto:fl.artyom.kuzkov@gmail.com">
+            email
+          </Link>
+          ,{' '}
+          <Link color="teal.500" href="https://github.com/kuzkov" isExternal>
+            my github page
+          </Link>
+        </Text>
+        <HStack mt="4">
+          <Button colorScheme="teal" as="a" href="https://t.me/artyom_kuzkov">
+            Contact me
+          </Button>
+          <Button colorScheme="teal" variant="ghost" as="a" href="/resume.pdf">
+            Learn more
+          </Button>
+        </HStack>
+      </Box>
     </Container>
   );
 };
